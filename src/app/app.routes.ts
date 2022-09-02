feb6dd41587d2e44e1effb2f1ae1b83fe8125c7b
Mary <maria.martinez580@misena.edu.co>
2022-09-02 15:38:33 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { SearchComponent } from './components/search/search.component';
 export const ROUTES: Routes=[
 {path: 'home', component: HomeComponent},
 {path: 'search', component: SearchComponent},
-{path:'', pathMatch: 'full', redirectTo:'home'},
+//{path:'', pathMatch: 'full', redirectTo:'home'},
 //QUE ME REDIRECCIONE CUALQUIER PATH AL HOME
 {path:'**', pathMatch:'full', redirectTo:'home'} 
 ];
