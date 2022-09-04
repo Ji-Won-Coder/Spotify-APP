a0da251f76bac71cf9493c1f0ebdda66da9baf78
kgnergns <martinezdahiana757@gmail.com>
2022-09-04 18:05:13 -0500

    resolviendo conflicto del la redireccion del router AJAJJA LO LOGRE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,6 @@ import { SearchComponent } from './components/search/search.component';
 export const ROUTES: Routes=[
 {path: 'home', component: HomeComponent},
 {path: 'search', component: SearchComponent},
-<<<<<<< HEAD
-{path:'', pathMatch: 'full', redirectTo:'home'},
-=======
-//{path:'', pathMatch: 'full', redirectTo:'home'},
->>>>>>> feb6dd41587d2e44e1effb2f1ae1b83fe8125c7b
-//QUE ME REDIRECCIONE CUALQUIER PATH AL HOME
+
 {path:'**', pathMatch:'full', redirectTo:'home'} 
 ];
